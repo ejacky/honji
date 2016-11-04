05e977aadae92a7009c92cbae0a62abf82441599
jackyzhang <852566802@qq.com>
2016-11-04 15:16:42 +0800

    update

---
+++ framework/Providers/ClassProvider.php
@@ -0,0 +1,36 @@
+<?php
+namespace Honji\Provider;
+
+use Pimple\ServiceProviderInterface;
+use Pimple\Container;
+
+class ClassProvider implements ServiceProviderInterface
+{
+    private $classes = [
+        'Core' => [
+            'Request',
+            'Response',
+            'RememberMeCookie',
+        ]
+    ];
+
+    public function register(Container $container)
+    {
+        $this->buildDIC($container, $this->classes);
+        return $this;
+    }
+
+    public function buildDIC(Container $container, array $namespaces)
+    {
+        foreach ($namespaces as $namespace => $classes) {
+            foreach ($classes as $name) {
+                $class = '\\Honji\\'.$namespace.'\\'.$name;
+                $container[lcfirst($name)] = function ($c) use ($class) {
+                    return new $class($c);
+                };
+            }
+        }
+
+        return $container;
+    }
+}
--- framework/Providers/RoutingProvider.php
+++ framework/Providers/RoutingProvider.php
@@ -3,13 +3,15 @@ namespace Honji\Provider;
 
 use Pimple\ServiceProviderInterface;
 use Pimple\Container;
+use Honji\Core\Router;
+use Honji\Core\Route;
 
 class RoutingProvider implements ServiceProviderInterface
 {
     public function register(Container $container)
     {
-        $container['router'] = new \Router($container);
-        $container['route'] = new \Route($container);
+        $container['router'] = new Router($container);
+        $container['route'] = new Route($container);
 
         return $container;
     }
