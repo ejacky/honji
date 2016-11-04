b659582cd4f5e5ca6b9862f32ac0d25acf5b9558
jackyzhang <852566802@qq.com>
2016-11-04 12:10:15 +0800

    add Request

---
+++ Request.php
@@ -0,0 +1,23 @@
+<?php
+class Request
+{
+    protected $query;
+
+    protected $post;
+
+    protected $server;
+
+    protected $cookie;
+
+    protected $file;
+
+
+    public function __construct()
+    {
+        $this->query = $_GET;
+        $this->post = $_GET;
+        $this->server = $_SERVER;
+        $this->file = $_FILES;
+        $this->cookie = $_COOKIE;
+    }
+}
