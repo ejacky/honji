c7ac047b189196fe406c7a9980ce7bf296e1c21d
jackyzhang <852566802@qq.com>
2016-11-04 16:40:04 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
 require_once __DIR__ . '/../bootstrap/autoload.php';
 
+try {
+    $container['router']->dispatch();
 
+} catch (Exception $e) {
+    echo "something wrong:" . $e->getMessage();
+}
